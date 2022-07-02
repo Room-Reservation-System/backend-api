7b3dafdb13883f2eb56c907fc0109361b2668935
ilkhom19 <ilkhomzhon.sidikov@gmail.com>
2022-07-02 10:36:00 +0600

    modified APIs 2

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('room/<int:id>', views.meeting_list), # GET, POST   :: id = class_id
-    path('meeting/<int:id>', views.meeting_detail), # PUT, DELETE :: id = meeting_id
+    path('room/<int:id>', views.meeting_list), # GET, POST   -->  id = class_id
+    path('meeting/<int:id>', views.meeting_detail), # GET, PUT, DELETE -->  id = meeting_id
     path('rooms/', views.room_list),                # GET rooms
 ]
