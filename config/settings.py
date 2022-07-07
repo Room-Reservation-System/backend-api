9115fc1e01163cc948fc09e5742d008577ffbe8b
oneku16 <eku.ulanov@yandex.com>
2022-07-07 18:44:19 +0600

    schedule -> inheritance universal

--- config/settings.py
+++ config/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'email_templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'email_templates'), os.path.join(BASE_DIR, 'config')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
