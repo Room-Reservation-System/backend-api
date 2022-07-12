d0d5133cf328dfe6faa899f0404eafe8b386a968
ilkhom19 <ilkhomzhon.sidikov@gmail.com>
2022-07-12 09:53:39 +0600

    added filters to admin and modified models

--- api/models.py
+++ api/models.py
@@ -12,13 +12,27 @@ class Meeting (models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     date = models.DateField(null=True)
+    created_at = models.DateTimeField(auto_now=True)
     start_time = models.TimeField(null=True)
     end_time = models.TimeField(null=True)
     room = models.ForeignKey('Room', on_delete=models.SET_NULL, null=True)
-    is_repeated = models.BooleanField(null=True)
+    is_repeated = models.BooleanField(default=False)
     verify_id = models.IntegerField(null=True)
     email = models.EmailField(null=True)
-    status = models.BooleanField(null=True)
+
+    MEETING_STATUS = (
+        ('p', 'Pending'),
+        ('d', 'Declined'),
+        ('a', 'Accepted'),
+    )
+    status = models.CharField(
+        max_length=1,
+        choices=MEETING_STATUS,
+        blank = True,
+        default = 'p',
+        help_text='Book availability')
 
     def __str__(self):
         return self.name
+    class Meta:
+        ordering = ['date']
--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ana^0nv(j($-#z!t5w*+35qy*x45pu2s0zc2v=a(k&@h+z2i7n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] 
 
 
 # Application definition
