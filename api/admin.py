97cb9d3c153418577ca1ce185141a017820f19ef
ilkhom <ilkhom.c@gmail.com>
2022-07-21 12:26:53 +0000

    fixed

--- api/admin.py
+++ api/admin.py
@@ -3,5 +3,15 @@ from .models import Meeting, Room
 
 # Register your models here.
 
-admin.site.register(Meeting)
-admin.site.register(Room)
+# admin.site.register(Meeting)
+# admin.site.register(Room)
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['name','information','id']
+
+class MeetingAdmin(admin.ModelAdmin):
+    list_display = ['name','description','room','date','start_time','end_time','is_repeated','email','status','created_at']
+    list_filter = ( 'room','is_repeated','date','status')
+
+admin.site.register(Room,RoomAdmin)
+admin.site.register(Meeting, MeetingAdmin)
--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ana^0nv(j($-#z!t5w*+35qy*x45pu2s0zc2v=a(k&@h+z2i7n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ilkhom19.pythonanywhere.com']
 
 
 # Application definition
@@ -39,10 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'api',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -108,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bishkek'
 
 USE_I18N = True
 
@@ -118,9 +122,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = "/home/ilkhom19/backend-api/static"
+# or, eg,
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ORIGIN_WHITELIST = (
+# 'http://localhost:3000',
+# 'http://localhost:8000',
+# )
+
+
