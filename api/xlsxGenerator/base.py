a375015b2bc880c1df590b22d25b9915b6af059d
oneku16 <eku.ulanov@yandex.com>
2022-07-29 01:14:33 +0600

    xlsx generator debuged, works well

--- api/xlsxGenerator/base.py
+++ api/xlsxGenerator/base.py
@@ -1,6 +1,29 @@
-from openpyxl import Workbook
-
+from openpyxl import Workbook, load_workbook
+from django.conf import settings
+from os import path, walk
 
 class Base:
-    wb=Workbook()
-    sheet=wb.active
+    
+    def __init__(self,filename:str):
+        self.fileName=filename
+        self.filePath=path.join(settings.BASE_DIR,'xlsxFiles',filename)
+        self.folderDir=path.join(settings.BASE_DIR,'xlsxFiles')
+        self.workBook=None
+
+    def getWorkSheet(self):
+        files=[]
+        for (dPath,dNames,dFiles) in walk(self.folderDir):
+            files.extend(dFiles)
+        if self.fileName in files:
+            with open(self.filePath, 'rb') as xlsx:
+                self.workBook=load_workbook(xlsx)
+                workSheet=self.workBook[self.workBook.sheetnames[0]]
+                return workSheet
+        else:
+            self.workBook=Workbook()
+            workSheet=self.workBook.active
+            return workSheet
+    
+    def saveXlsx(self):
+        self.workBook.save(filename=self.filePath)
+        self.workBook.close()
